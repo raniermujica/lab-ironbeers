a70d790365556303e3bd4a99b344ad647dcecccc
Ranier Mujica <ranieremujica@gmail.com>
2022-10-13 02:27:36 +0200

    solved lab with errors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -6,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+
+//const randomBeer = PunkAPI.getRandom()
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +20,86 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+//Método para obtener las cervezas de la API
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(response);
+
+      let filterBeers = beersFromApi.map(
+        ({ image_url, name, description, tagline }) => ({
+          image_url,
+          name,
+          description,
+          tagline
+        })
+      );
+      console.log(filterBeers);
+
+      //let beerImageArr = Object.keys(response.name);
+      //  console.log(beerImageArr);
+      let allBeers = Object.values(filterBeers);
+      console.log(allBeers);
+
+      res.render('beers.hbs', {
+        allBeers: allBeers,
+        name: allBeers.name,
+        description: allBeers.description,
+        tagline: allBeers.tagline
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//----------------------------------------------------
+//Random Beer
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log(responseFromAPI);
+
+      let randonFilterBeers = responseFromAPI.map(
+        ({
+          image_url,
+          name,
+          description,
+          tagline,
+          food_pairing,
+          brewer_tips
+        }) => ({
+          image_url,
+          name,
+          description,
+          tagline,
+          food_pairing,
+          brewer_tips
+        })
+      );
+      console.log(randonFilterBeers);
+
+      let randomItem = Object.values(randonFilterBeers);
+      console.log(randomItem);
+
+      res.render('randomBeer.hbs', {
+        randomItem: randomItem,
+        name: randomItem.name,
+        description: randomItem.description,
+        tagline: randomItem.tagline,
+        //food_pairing: food_pairing,
+        //brewer_tips: brewer_tips,
+        //!falta hacer un bucle para iteractuar con el array de food_pairing
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//-----------------------------------------------------
 
 app.get('/', (req, res) => {
   res.render('index');
